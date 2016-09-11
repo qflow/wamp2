b7454af29de709739cc6a000abe1c69e6020c053
mfojtak <mfojtak@seznam.cz>
2016-09-11 20:15:41 +0200

    reg

---
+++ registration.h
@@ -0,0 +1,18 @@
+#ifndef REGISTRATION_H
+#define REGISTRATION_H
+
+
+namespace qflow{
+class registration
+{
+
+};
+template<typename T>
+class registration_impl : public registration
+{
+
+};
+
+}
+
+#endif
