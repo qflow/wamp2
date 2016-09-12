3575e21f901d14b8a1b6f71ec70fe5632b32fd9c
mfojtak <mfojtak@seznam.cz>
2016-09-12 20:26:05 +0200

    registration

--- registration.h
+++ registration.h
@@ -1,16 +1,33 @@
 #ifndef REGISTRATION_H
 #define REGISTRATION_H
 
+#include <type_traits>
+#include "util/function_traits.h"
+#include <iostream>
 
 namespace qflow{
 class registration
 {
 
 };
-template<typename T>
+template<typename T, typename Enable = void>
 class registration_impl : public registration
 {
 
+};
+template<typename T>
+class registration_impl<T, std::enable_if_t<function_traits<T>::is_callable>>
+{
+public:
+    registration_impl(T&& f) : _func(f)
+    {
+    }
+    void test()
+    {
+
+    }
+private:
+    T _func;
 };
 
 }
