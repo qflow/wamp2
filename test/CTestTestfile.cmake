c44badd6bc51f9288cb909da74ec62bc1d229db3
mfojtak <mfojtak@seznam.cz>
2016-11-15 08:01:36 +0100

    changes

--- test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/workspace/wamp2/test
-# Build directory: D:/workspace/wamp2/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(wamp.test.all "test_all")
